125defa0cc54c50b9bcd261c969534d4cbe6a24c
KFourCS <nonutty29@gmail.com>
2024-07-09 10:25:57 -0700

    Update dns_resolver.py

--- src/commands/dns_resolver.py
+++ src/commands/dns_resolver.py
@@ -1,22 +1,4 @@
 from pydoc import resolve
 
-import aiodns
-
-
-async def resolve_dns(hostname):
-    try:
-        result = await resolve.query(hostname, 'A')
-        ipv4_addresses = [response.host for response in result] if result else []
-
-        result_v6 = await resolve.query(hostname, 'AAAA')
-        ipv6_addresses = [response.host for response in result_v6] if result_v6 else []
-
-        return ipv4_addresses, ipv6_addresses
-
-    except aiodns.error.DNSError as e:
-        print(f"DNS resolution failed for {hostname}: {e}")
-        return [], []
-
-    except Exception as e:
-        print(f"An error occurred during DNS resolution for {hostname}: {e}")
-        return [], []
+async def resolve_dns(message):
+    await message.channel.send("What DNS do you want to resolve?");
