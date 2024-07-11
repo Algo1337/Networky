d1c673c73f984cb37434aa25abe65d006f33b5b7
KFourCS <nonutty29@gmail.com>
2024-07-10 22:28:19 -0700

    Update geo.py

--- src/commands/geo.py
+++ src/commands/geo.py
@@ -1,6 +1,7 @@
 import discord
 import requests
 from discord import Embed
+from datetime import datetime
 from ..discord_utils.messages import *
 
 async def geo(message: discord.Message):
@@ -19,5 +20,24 @@ async def geo(message: discord.Message):
         await message.channel.send(embed=embed)
         return 
     
-    embed = Embed(title="Geolocation Information", description=resp.text, color=discord.Color.blue())
+    data = resp.json()
+    
+    embed = Embed(title="Geolocation Information", color=discord.Color.blue())
+    embed.add_field(name="IP", value=data["ip"], inline=True)
+    embed.add_field(name="Hostname", value=data["hostname"], inline=True)
+    embed.add_field(name="City", value=data["city"], inline=True)
+    embed.add_field(name="Region", value=data["region"], inline=True)
+    embed.add_field(name="Country", value=data["country"], inline=True)
+    embed.add_field(name="ISP", value=data["isp"], inline=True)
+    
+    image_url = "https://www.freepnglogos.com/uploads/globe-png/globe-telecom-septier-15.png"
+    embed.set_image(url=image_url)
+    
+    copyright_url = "https://www.citypng.com/public/uploads/preview/hd-copyright-all-rights-reserved-red-stamp-png-11662464508s8pru5hcxm.png"
+    embed.set_footer(text="Networky 2024 ©", icon_url=copyright_url)
+    
+    embed.timestamp = datetime.utcnow()
+    
+    await message.channel.send(embed=embed)
+
     await message.channel.send(embed=embed)
