4ff5d90c5842884eb9df8616bc1eb4d6b91bfdc0
KFourCS <nonutty29@gmail.com>
2024-07-10 22:02:34 -0700

    Update geo.py

--- src/commands/geo.py
+++ src/commands/geo.py
@@ -1,13 +1,23 @@
-import discord, requests
-
+import discord
+import requests
+from discord import Embed
 from ..discord_utils.messages import *
 
-async def geo(message: discord.message):
+async def geo(message: discord.Message):
     msg = Message(message)
 
     if len(msg.args) < 2:
-        await message.channel.send("[ X ] Error, Invalid arguments\nUsage: +geo <ip>")
+        embed = Embed(title="[ X ] Error, Invalid arguments", color=discord.Color.red())
+        embed.add_field(name="Usage", value="+geo <ip>")
+        await message.channel.send(embed=embed)
         return 
     
     resp = requests.get(f"https://ipwho.is/{msg.args[1]}")
-    await message.channel.send(resp.text)
+    
+    if resp.status_code != 200:
+        embed = Embed(title="Error", description="An error occurred while fetching geo information.", color=discord.Color.red())
+        await message.channel.send(embed=embed)
+        return 
+    
+    embed = Embed(title="Geolocation Information", description=resp.text, color=discord.Color.blue())
+    await message.channel.send(embed=embed)
