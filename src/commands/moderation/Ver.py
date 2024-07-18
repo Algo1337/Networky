4d00ef66ede0cb36819ce5b3952aefeeba3cb2d6
KFourCS <nonutty29@gmail.com>
2024-07-17 18:35:16 -0700

    Update Ver.py

--- src/commands/moderation/Ver.py
+++ src/commands/moderation/Ver.py
@@ -6,11 +6,12 @@ from ...discord_utils.messages import *
 
 async def ver(message: discord.message):
     
-    embed = Embed(title="Bot Info", description="List Of Bot Information", color=discord.Color.green())
+    embed = Embed(title="Click Here For Latest Update!", description="Bot Information", color=discord.Color.green())
     embed.add_field(name="Bot Version", value="v1.0", inline=True)
     embed.add_field(name="Coded In", value="Python", inline=True)
     embed.add_field(name="Coded By", value="Networky Dev Team", inline=True)
     embed.add_field(name="Need Help?", value="Use The Help Command For Help (+help)", inline=False)
+    embed.url = "https://youtube.com/"
     embed.set_image(url="https://shorturl.at/CLtQb")
     embed.set_footer(text="Thanks For Using The Networky Moderation Bot.")
 
