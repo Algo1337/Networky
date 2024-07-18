1aa6b09f9e1ae1651fd8b25e3a79ea33ee6dfc4c
KFourCS <nonutty29@gmail.com>
2024-07-17 18:31:04 -0700

    Update Ver.py

--- src/commands/moderation/Ver.py
+++ src/commands/moderation/Ver.py
@@ -1,12 +1,18 @@
 import discord
+
 from discord import Embed
 from ...discord_utils.messages import *
 
 
-def Ver(message: discord.message):
+async def ver(message: discord.message):
     
     embed = Embed(title="Bot Info", description="List Of Bot Information", color=discord.Color.green())
-    embed.add_field(name="Bot Version", value="v1.0", inline=False)
-    embed.add_field(name="Coded In", value="Python", inline=False)
-    embed.add_field(name="Coded By", value="Networky Dev Team", inline=False)
+    embed.add_field(name="Bot Version", value="v1.0", inline=True)
+    embed.add_field(name="Coded In", value="Python", inline=True)
+    embed.add_field(name="Coded By", value="Networky Dev Team", inline=True)
+    embed.add_field(name="Need Help?", value="Use The Help Command For Help (+help)", inline=False)
+    embed.set_image(url="https://shorturl.at/CLtQb")
     embed.set_footer(text="Thanks For Using The Networky Moderation Bot.")
+
+
+    await message.channel.send(embed=embed)
