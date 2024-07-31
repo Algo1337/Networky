5764a847d5767c791e0e9b640d9405af6dea75c4
KFourCS <nonutty29@gmail.com>
2024-07-30 23:32:46 -0700

    Update nuke.py

--- src/commands/moderation/nuke.py
+++ src/commands/moderation/nuke.py
@@ -3,14 +3,22 @@ import discord
 from discord import guild
 from ...utils.discord.messages import *
 
-async def nuke(message: discord.message):
+async def nuke(message: discord.Message):
     channel = message.channel
 
     if not message.author.guild_permissions.administrator:
         await message.channel.send("You're not an admin sir!")
-        
+        return
+
     new_channel = await channel.clone()
     await channel.delete()
+
     await new_channel.edit(position=channel.position)
 
-    await new_channel.send(f"HOLY SHIT, WE JUST GOT FUCKING HACKED")
+    embed = discord.Embed(
+        title="🚨 Attention 🚨",
+        description="HOLY SHIT, WE JUST GOT FUCKING HACKED",
+        color=discord.Color.red()
+    )
+
+    await new_channel.send(embed=embed)
